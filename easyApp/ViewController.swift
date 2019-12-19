8b55ff9692c032a5567d1542cb4307d90b3b8913
kevin saliu <kevin_sa@ymail.com>
2019-12-19 11:47:57 +0100

    KevinsCommit
    Scanner works
    Restraints in place for results

---
+++ easyApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  easyApp
+//
+//  Created by tbidn001 on 12.12.19.
+//  Copyright © 2019 tbidn001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
